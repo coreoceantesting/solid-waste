254aceed0991014c70425aabaae6764ed75418b5
coreoceantesting <coreoceantesting@gmail.com>
2025-01-22 18:51:10 +0530

    chnges

--- app/Http/Controllers/Admin/Masters/ContractMappingController.php
+++ app/Http/Controllers/Admin/Masters/ContractMappingController.php
@@ -35,16 +35,12 @@ class ContractMappingController extends Controller
         if ($ZonePrefix) {
             $ZoneDetails = DB::table('prefix_details')->where('Main_Prefix', $ZonePrefix->id)->get();
         }
-        dd('ZoneDetails');
-
-
 // Retrieve Waste Type Prefix Details
         $WasteTypePrefix = DB::table('prefixes')->where('Prefix_Name', 'WST')->first();
         $WasteTypeDetails = [];
         if ($WasteTypePrefix) {
             $WasteTypeDetails = DB::table('prefix_details')->where('Main_Prefix', $WasteTypePrefix->id)->get();
         }
-        dd('WasteTypeDetails');
         return view('admin.masters.contractMapping')->with(['ContractMapping'=> $ContractMapping,'TaskMapping'=> $taskmappings,'SlrmEmployeeDetails'=>$SlrmEmployeeDetails,'CapacityOfVehicle'=>$CapacityOfVehicle,'Ward'=>$Ward, 'ZonePrefix'=>$ZonePrefix,'ZoneDetails'=>$ZoneDetails,'WasteTypePrefix'=>$WasteTypePrefix,'WasteTypeDetails'=>$WasteTypeDetails]);
          }
 
