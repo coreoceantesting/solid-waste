00a6b810e2c087e512165b5be986f3511c2aa497
coreoceantesting <coreoceantesting@gmail.com>
2025-01-23 14:03:33 +0530

    chngesssss

--- resources/views/admin/masters/contractMapping.blade.php
+++ resources/views/admin/masters/contractMapping.blade.php
@@ -638,12 +638,12 @@
                         $.each(response.TaskMapping, function(index, task) {
                             additionalDetailsHtml += `
                                 <tr>
-                                    <td>${task.zone_value || 'N/A'}</td>
+                                    <td>${task.zone || 'N/A'}</td>
                                     <td>${task.ward || 'N/A'}</td>
                                     <td>${task.colony || 'N/A'}</td>
                                     <td>${task.society || 'N/A'}</td>
                                     <td>${task.task || 'N/A'}</td>
-                                    <td>${task.waste_type_value || 'N/A'}</td>
+                                    <td>${task.waste_type || 'N/A'}</td>
                                     <td>${task.garbage_volume || 'N/A'}</td>
                                     <td>${task.beat_number || 'N/A'}</td>
                                     <td>${task.employee_count || 'N/A'}</td>
