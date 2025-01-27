717158b5a440e6ca84f85f2e35531331ba5bebba
coreoceantesting <coreoceantesting@gmail.com>
2025-01-27 14:54:07 +0530

    changes in the tripsheet blade form

--- resources/views/admin/reports/trip-sheet-report.blade.php
+++ resources/views/admin/reports/trip-sheet-report.blade.php
@@ -20,12 +20,12 @@
                                 <th>In Time</th>
                                 <th>Out Time</th>
                                 <th>Entry Weight</th>
-                                <th>Exit Weight</th>
+                                {{-- <th>Exit Weight</th>
                                 <th>Total Garbage</th>
                                 <th>Driver Name</th>
                                 <th>Weight Slip Number</th>
                                 <th>File Upload</th>
-                                <th>Waste Segregated</th>
+                                <th>Waste Segregated</th> --}}
                             </tr>
                         </thead>
                         <tbody>
@@ -39,12 +39,12 @@
                             <td>{{$trip->in_time}}</td>
                             <td>{{$trip->out_time}}</td>
                             <td>{{$trip->entry_weight}}</td>
-                            <td>{{$trip->exit_weight}}</td>
+                            {{-- <td>{{$trip->exit_weight}}</td>
                             <td>{{$trip->total_garbage}}</td>
                             <td>{{$trip->driver_name}}</td>
                             <td>{{$trip->weight_slip_number}}</td>
                             <td>{{$trip->file_upload}}</td>
-                            <td>{{$trip->waste_segregated}}</td>
+                            <td>{{$trip->waste_segregated}}</td> --}}
                             </tr>
                            @endforeach
                         </tbody>
