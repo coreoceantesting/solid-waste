d8b965cde0f2bd3c81251aeb8676271d29fcc583
coreoceantesting <coreoceantesting@gmail.com>
2025-02-15 18:34:13 +0530

    chngesss in soli waste

--- app/Http/Requests/Admin/Masters/StoreContractMapping.php
+++ app/Http/Requests/Admin/Masters/StoreContractMapping.php
@@ -22,12 +22,16 @@ class StoreContractMapping extends FormRequest
     public function rules(): array
     {
         return [
-            // 'contract_number' => 'required',
-            'contract_number' => 'required|unique:contract_mappings,contract_number,NULL,NULL,deleted_at,NULL',
-            'contract_name' => 'required|unique:contract_mappings,contract_name,NULL,NULL,deleted_at,NULL',
-            'contact_number'=>'required|unique:contract_mappings,contact_number,NULL,NULL,deleted_at,NULL',
-            'contract_date'=>'required|unique:contract_mappings,contract_date,NULL,NULL,deleted_at,NULL',
-            'expiry_date'=>'required|unique:contract_mappings,expiry_date,NULL,NULL,deleted_at,NULL',
+            'contract_number' => 'required',
+            'contract_name'=>'required',
+            'contact_number'=>'required',
+            'contract_date'=>'required',
+            'expiry_date'=>'required',
+            // 'contract_number' => 'required|unique:contract_mappings,contract_number,NULL,NULL,deleted_at,NULL',
+            // 'contract_name' => 'required|unique:contract_mappings,contract_name,NULL,NULL,deleted_at,NULL',
+            // 'contact_number'=>'required|unique:contract_mappings,contact_number,NULL,NULL,deleted_at,NULL',
+            // 'contract_date'=>'required|unique:contract_mappings,contract_date,NULL,NULL,deleted_at,NULL',
+            // 'expiry_date'=>'required|unique:contract_mappings,expiry_date,NULL,NULL,deleted_at,NULL',
             'zone' => 'required',
             'ward' => 'required',
             'colony' => 'required',
